65cc3ef8b764dc79d8439b5e51315a07669c22a7
KelvinYou <ykekelvin0220@gmail.com>
2023-03-09 16:55:52 +0800

    submit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,9 +27,14 @@ const Home = () => {
       <Button variant='contained'
       onClick={() => { navigate("/todos") }}>Advance</Button>
 
-      <div>Interviewee: Kelvin</div>
+      <div>Interviewee: Kelvin You</div>
       <div>Start at: 2.08pm</div>
       <div>Done at: 4.49pm</div>
+      <div>Pushed to github at: 4.51pm</div>
+      <div>github: <a href="https://github.com/KelvinYou/interview-react" target="_blank">https://github.com/KelvinYou/interview-react</a></div>
+      <div>Deploy at: 4.52pm</div>
+      <div>link: <a href="https://interview-react-orcin.vercel.app/" target="_blank">https://interview-react-orcin.vercel.app/</a></div>
+      <div>Submit to HOD at: 4.55pm</div>
     </div>
   )
 }
